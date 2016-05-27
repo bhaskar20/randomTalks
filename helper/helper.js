d9d66acd3f5604e0789b4377d925bc8bfc0d15cc
bhaskar <sharmabhaskar13@gmail.com>
2016-05-27 14:48:43 +0530

    adding test echo hooks
    Signed-off-by: bhaskar <sharmabhaskar13@gmail.com>

---
+++ helper/helper.js
@@ -0,0 +1,23 @@
+var helper = {};
+
+helper.sendMessage = function(id,msg) {
+	request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: {"text" : msg}
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        } else {
+        	console.log("sent");
+        }
+    });
+}
+
+module.exports = helper;
--- routes/webhook.js
+++ routes/webhook.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var helper = require("../helper/helper.js");
 
 router.get('/', function(req, res) {
   if (req.query['hub.verify_token'] === "Random_talks_secured") {
@@ -8,6 +9,20 @@ router.get('/', function(req, res) {
   	console.log("failed token verification");
     res.send('Error, wrong validation token');    
   }
+});
+
+router.post('/', function (req, res) {
+    var events = req.body.entry[0].messaging;
+    console.log(events);
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+        	console.log("HERE IS THE MESSAGE ===>> "+event+event.message);
+        	//do soemthing here
+            helper.sendMessage(event.sender.id,"Echo: "+event.message.text);
+        }
+    }
+    res.sendStatus(200);
 });
 
 
