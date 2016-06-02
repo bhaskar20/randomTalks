9b536bb00c5e7c17c9c8cedeb99bbb453f573fe3
bhaskar <sharmabhaskar13@gmail.com>
2016-06-02 14:57:42 +0530

    add policy
    Signed-off-by: bhaskar <sharmabhaskar13@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.send({"message":"Sorry this is reserved for now"});
+});
+
+router.get('/policy',function(req, res){
+	var pa = path.join(__dirname+"../public/views/policy.html");
+	res.sendFile(pa);
 });
 
 module.exports = router;
